b4afe7368986527715a5f156b1b0c4592b9590a1
tgmerge <tgmgsp@gmail.com>
2017-07-07 23:17:49 +0800

    update before removing local project folder

--- Assembler.bat
+++ Assembler.bat
@@ -1,3 +1,3 @@
 @echo off
-java -jar Assembler.jar test.asm
+"c:\Program Files (x86)\Java\jre7\bin\java.exe" -jar Assembler.jar test.asm
 pause
--- test.asm
+++ test.asm
@@ -1,7 +1,22 @@
-direct 4096
-addi $t1, $zero, 10
-addi $t2, $zero, 10
-beq  $t1, $t2, skip
-add  $t3, $t1, $t2
-skip:
-add  $t3, $zero, $t1
+begin:
+         lw $1, 20($0)         
+         lw  $2, 21($0)        
+         lw  $3, 22($0)        
+         lw  $4, 23($0)        
+         add $5, $2, $2       
+         addiu $6, $2, -1      
+         lui $7, 1024           
+         xor  $7, $7, $4     
+         slti $7, $7, -1       
+         sltiu $7, $7, -1      
+         beq $1, $2, skip1     
+         addi $1, $0, 4        
+         beq $1, $3, skip1     
+         addi $0, $0, 0        
+skip1:
+         addi $1, $0, 8        
+skip3:
+         beq $0, $0, skip2       
+skip2:
+         beq $0, $0, skip3
+         j skip1
